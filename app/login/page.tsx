7bdcd0ec6002091b5e2fb792ed2eb825f0507a91
Andru <andruraykah@gmail.com>
2024-11-07 14:43:33 -0300

    feat: add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+    const { userId } = await auth();
+    if (userId) {
+        redirect('/')
+    }
     return (
         <div className="grid h-full grid-cols-2">
             {/* ESQUERDA */}
@@ -12,10 +19,12 @@ const LoginPage = () => {
                 <p className="text-muted-foreground mb-8">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit.
                 </p>
-                <Button variant="outline">
-                    <LogInIcon className="mr-2"/>
-                    Fazer login ou criar conta
-                </Button>
+                <SignInButton>
+                    <Button variant="outline">
+                        <LogInIcon className="mr-2"/>
+                        Fazer login ou criar conta
+                    </Button>
+                </SignInButton>
             </div>
             
 
