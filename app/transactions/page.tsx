27dfe85c3768efe311a5f91a12ee884c69fac5a1
Andru <andruraykah@gmail.com>
2024-11-10 03:49:27 -0300

    chore: dd user id property to transcation table

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,14 @@
-const TransactionPage = () => {
-  return <h1>Transactions</h1>;
+import { db } from "../_lib/prisma";
+
+const TransactionPage = async () => {
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div>
+      {transactions.map((transaction) => (
+        <div key={transaction.id}>{transaction.name}</div>
+      ))}
+    </div>
+  );
 };
 
 export default TransactionPage;
